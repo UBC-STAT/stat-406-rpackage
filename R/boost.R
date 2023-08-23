26a5c181be5a6d4d8e413b7f93693452068df399
Daniel J. McDonald <dajmcdon@gmail.com>
2023-08-23 09:14:43 -0700

    add boosting data for lab 8

---
+++ R/boost.R
@@ -0,0 +1,29 @@
+#' Anonymous data for boosting exploration
+#'
+#' @description
+#' This is some made up data for exploring the performance of boosting.
+#' The variables have been renamed and the class labels altered to remove
+#' any historical associations with some other data that we try to avoid
+#' using.
+#'
+#' @description
+#' For both datasets, column names are `x`, `y`, and `class`.
+#'
+#' @details
+#'
+#' `goodboost` is a case where stump-based boosting works well.
+#'
+#' @keywords datasets
+#' @examples
+#' goodboost
+#'
+#' badboost
+#'
+"goodboost"
+
+
+#' @details
+#' `badboost` is a case where stump-based boosting works poorly
+#'
+#' @rdname goodboost
+"badboost"
