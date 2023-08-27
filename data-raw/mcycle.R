bfd55d22e91715c10ae5ebe9b844efc7713d304c
Daniel J. McDonald <dajmcdon@gmail.com>
2023-08-27 09:23:50 -0700

    tibble the mcycle data

--- data-raw/mcycle.R
+++ data-raw/mcycle.R
@@ -2,8 +2,8 @@
 m <- MASS::mcycle
 n <- nrow(m)
 sets <- c(0, rep(1:3, length = n - 2), 0)
-mcycle_test <- m[sets == 2, ]
-mcycle_train <- m[sets != 2, ]
+mcycle_test <- tibble::as_tibble(m[sets == 2, ])
+mcycle_train <- tibble::as_tibble(m[sets != 2, ])
 
 usethis::use_data(mcycle_test, overwrite = TRUE)
 usethis::use_data(mcycle_train, overwrite = TRUE)
